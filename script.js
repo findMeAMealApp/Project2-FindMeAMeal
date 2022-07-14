70c85e9673b6925470c82c5708daeeb4f8eacb4e
CRobock <crobock07@gmail.com>
2022-07-14 17:46:40 -0400

    udpated styles and added some JS

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-//search for meal from api
-//search random meal
-// let search = 'chicken'
+// Find Me A Meal App
 
-document.querySelector('form').addEventListener('submit', function (e) {
-     e.preventDefault()
-     const search = document.querySelector('input')
+// Global Variables
+const search = document.getElementById('search'),
+random = document.getElementById('random'),
+submit = document.getElementById('submit'),
+mealsEl = document.getElementById('meals'),
+resultHeadingEl = document.getElementById('result-heading'),
+singleMealEL = document.getElementById('single-meal');
 
-     // lets search meal by keyword
-     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`)
-          .then((res) => res.json())
-          .then((data) => {
-               console.log('FETCHING MEAL BY KEYWORD', data)
-          })
-})
+
+
+function searchMeal(e){
+     e.preventDefault();
+     const term = search;
+     console.log(term.value.trim());
+}
+
+submit.addEventListener('submit', searchMeal)
+
+// document.querySelector('form').addEventListener('submit', function (e) {
+//      e.preventDefault()
+//      search = document.querySelector('input')
+
+//      // API lets search meal by keyword
+//      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`)
+//           .then((res) => res.json())
+//           .then((data) => {
+//                console.log('FETCHING MEAL BY KEYWORD', data)
+//           })
+// })
+
+// Namespace FindMeAMeal
+
+const mealApp = {}
+
+// 
+
+// 
 
 //  for searching meal by id
-const mealId = 52850
-fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-     .then((res) => res.json())
-     .then((data) => {
-          console.log('FETCHING MEAL BY ID', data)
-     })
+// const mealId = 52850
+// fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+//      .then((res) => res.json())
+//      .then((data) => {
+//           console.log('FETCHING MEAL BY ID', data)
+//      })
 
-// for searching meal randomly
-fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-     .then((res) => res.json())
-     .then((data) => {
-          console.log('FETCHING MEAL RANDOMLY', data)
-     })
+// // for searching meal randomly
+// fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+//      .then((res) => res.json())
+//      .then((data) => {
+//           console.log('FETCHING MEAL RANDOMLY', data)
+//      })
