1a857df5e8c6c48aee4127ae522bdb93956f20cb
Akalpit <skininja673@gmail.com>
2022-07-18 17:23:33 -0400

    added the hover over functionality

--- script.js
+++ script.js
@@ -2,22 +2,56 @@
 
 // Global Variables
 const search = document.getElementById('search'),
-random = document.getElementById('random'),
-submit = document.getElementById('submit'),
-mealsEl = document.getElementById('meals'),
-resultHeadingEl = document.getElementById('result-heading'),
-singleMealEL = document.getElementById('single-meal');
+     random = document.getElementById('random'),
+     submit = document.getElementById('submit'),
+     mealsEl = document.getElementById('meals'),
+     resultHeadingEl = document.getElementById('result-heading'),
+     singleMealEL = document.getElementById('single-meal')
 
+const mealApp = {}
+mealApp.url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`
 
+function searchMeal(e) {
+     e.preventDefault()
+     const term = search.value.trim()
 
-function searchMeal(e){
-     e.preventDefault();
-     const term = search;
-     console.log(term.value.trim());
+     //hit the api only if term is not empty
+     if (term) {
+          fetch(mealApp.url)
+               .then((response) => {
+                    return response.json()
+               })
+               .then((jsonResponse) => {
+                    resultHeadingEl.innerHTML = `<h2>${term} meals: </h2>`
+
+                    const meals = jsonResponse.meals
+                    meals.forEach((meal) => {
+                         // console.log(meal)
+                         const imgDiv = document.createElement('div')
+                         imgDiv.className = 'meal'
+                         imgDiv.innerHTML = `
+                                <img src = "${meal.strMealThumb}">
+                                <div class = "meal-info">
+                                      <h3> ${meal.strMeal}</h3>
+                                </div>                              
+                              `
+                         console.log(imgDiv);
+
+                         //appending the parent meals
+                         mealsEl.appendChild(imgDiv)
+                    })
+               })
+
+          //clears the text value
+          search.value = ''
+     } else {
+          alert('Please provide your desired food')
+     }
 }
 
 submit.addEventListener('submit', searchMeal)
 
+// -------------------------------------------------
 // document.querySelector('form').addEventListener('submit', function (e) {
 //      e.preventDefault()
 //      search = document.querySelector('input')
@@ -31,12 +65,6 @@ submit.addEventListener('submit', searchMeal)
 // })
 
 // Namespace FindMeAMeal
-
-const mealApp = {}
-
-// 
-
-// 
 
 //  for searching meal by id
 // const mealId = 52850
